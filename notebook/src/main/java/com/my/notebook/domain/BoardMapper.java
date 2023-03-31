c196ec24445520a4bc998e09ac60a88ce3d55a9d
김병주 <atker14@gmail.com>
2023-03-31 17:46:49 +0900

    시작

---
+++ notebook/src/main/java/com/my/notebook/domain/BoardMapper.java
@@ -0,0 +1,9 @@
+package com.my.notebook.domain;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BoardMapper {
+
+    String getTest();
+}
---
+++ notebook/src/test/java/com/my/notebook/NotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
